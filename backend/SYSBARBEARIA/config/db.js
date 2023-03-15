31015ab2eddb613c324e5a5afdaf8d406ec3db65
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-03-15 12:57:40 -0300

    Adicionando classe de conexao com banco de dados

---
+++ backend/SYSBARBEARIA/config/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import Aluno from "../models/Aluno.js";
+import bcrypt from "bcryptjs";
+
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL_LOCAL);
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", async () => {
+    console.log("Desconectado do MongoDB.");
+});
+
+mongoose.connection.on("connected", async () => {
+    await populateDatabase();
+    console.log("Conectado ao MongoDB.");
+});
+
+mongoose.connection.on("error", async (error) => {
+    console.log(`Erro no MongoDB:\n${error}`);
+});
+
+export default connectDatabase;

9c8f039b58a353427796562b275e9c1ae01ca18d
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-03-19 16:19:59 -0300

    Adicionando novas funcoes a alguns repositorios

--- api/src/main/java/com/sysbarbearia/api/repositories/ServicoRepository.java
+++ api/src/main/java/com/sysbarbearia/api/repositories/ServicoRepository.java
@@ -1,11 +1,17 @@
 package com.sysbarbearia.api.repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sysbarbearia.api.model.*;
 
 @Repository
 public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-
+	@Transactional(readOnly = true)
+	@Query(value = "SELECT COUTN(*) FROM SERVICOS" , nativeQuery = true)
+    public Collection<?> countServicos();
 }
--- api/src/main/java/com/sysbarbearia/api/repositories/UsuarioRepository.java
+++ api/src/main/java/com/sysbarbearia/api/repositories/UsuarioRepository.java
@@ -1,11 +1,17 @@
 package com.sysbarbearia.api.repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sysbarbearia.api.model.*;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
+	@Transactional(readOnly = true)
+	@Query(value = "SELECT COUTN(*) FROM USUARIO" , nativeQuery = true)
+    public Collection<?> countUsuarios();
 }

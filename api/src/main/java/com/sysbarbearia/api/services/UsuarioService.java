571e91a58f5b5c95ee0d6fb3887bf85b3b712d3d
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-03-19 09:54:54 -0300

    Adicionando service para Usuario

---
+++ api/src/main/java/com/sysbarbearia/api/services/UsuarioService.java
@@ -0,0 +1,59 @@
+package com.sysbarbearia.api.services;
+import java.util.Collection;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sysbarbearia.api.repositories.*;
+import com.sysbarbearia.api.model.Usuario;
+import com.sysbarbearia.api.services.exceptions.*;
+
+@Service
+public class UsuarioService {
+	@Autowired
+	private UsuarioRepository repository;
+
+	public Usuario findById(Integer id)
+	{
+		Usuario obj = repository.findById(id).get();
+		if(obj == null)
+		{
+			throw new ObjectNotFoundException("Objeto nao encontrado Id: " + id + ", Tipo: " + Usuario.class.getName());
+		}
+		return obj;
+	}
+	
+	public Collection<Usuario> findAll()
+	{
+		return repository.findAll();
+	}
+	
+	public Usuario insert(Usuario obj)
+	{
+		obj.setIdUsuario(null);
+		return repository.save(obj);		
+	}
+	
+	public Usuario update(Usuario obj)
+	{
+		findById(obj.getIdUsuario());
+        try 
+        {
+        	return repository.save(obj);
+        }
+        catch (DataIntegrityViolationException e) 
+        {
+            throw new DataIntegrityException("Campo(s) obrigatório(s) do Usuario não foi(foram) preenchido(s)");
+        }
+    }
+	public void delete(Integer id) 
+	{
+        findById(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir um Usuario");
+        }
+    }
+	
+}

553ae56df519eb073c5219b4ff155a1a56603fe5
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-03-26 10:23:39 -0300

    Melhorando controller de servico oferecido

--- api/src/main/java/com/sysbarbearia/api/controllers/ServicoOferecidoController.java
+++ api/src/main/java/com/sysbarbearia/api/controllers/ServicoOferecidoController.java
@@ -13,30 +13,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sysbarbearia.api.model.Barbearia;
-import com.sysbarbearia.api.services.EmpresaService;
+import com.sysbarbearia.api.model.ServicoOferecido;
+import com.sysbarbearia.api.services.ServicoOferecidoService;
 import com.sysbarbearia.api.services.exceptions.ConstraintException;
 
 @RestController
 @RequestMapping(value = "/servicosoferecidos")
 public class ServicoOferecidoController {
 	@Autowired
-	private EmpresaService service;
+	private ServicoOferecidoService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<Collection<Barbearia>> findAll() {
-        Collection<Barbearia> collection = service.findAll();
+    public ResponseEntity<Collection<ServicoOferecido>> findAll() {
+        Collection<ServicoOferecido> collection = service.findAll();
         return ResponseEntity.ok().body(collection);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Barbearia> find(@PathVariable Integer id) {
-        Barbearia obj = service.findById(id);
+    public ResponseEntity<ServicoOferecido> find(@PathVariable Integer id) {
+        ServicoOferecido obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
         
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Barbearia> insert(@Valid @RequestBody Barbearia obj, BindingResult br) {
+    public ResponseEntity<ServicoOferecido> insert(@Valid @RequestBody ServicoOferecido obj, BindingResult br) {
         if (br.hasErrors())
         	throw new ConstraintException(br.getAllErrors().get(0).getDefaultMessage());
         obj = service.insert(obj);
@@ -44,7 +44,7 @@ public class ServicoOferecidoController {
     }
     
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Barbearia> update(@Valid @RequestBody Barbearia obj, BindingResult br) {
+    public ResponseEntity<ServicoOferecido> update(@Valid @RequestBody ServicoOferecido obj, BindingResult br) {
     	if (br.hasErrors())
         	throw new ConstraintException(br.getAllErrors().get(0).getDefaultMessage());
         obj = service.update(obj);

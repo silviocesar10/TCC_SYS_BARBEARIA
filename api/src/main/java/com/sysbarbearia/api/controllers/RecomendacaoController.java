06f558b0f196509cd7ecdfabda98e55aba618d09
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-04-17 15:58:32 -0300

    Atualizando RecomendacaoController

--- api/src/main/java/com/sysbarbearia/api/controllers/RecomendacaoController.java
+++ api/src/main/java/com/sysbarbearia/api/controllers/RecomendacaoController.java
@@ -2,6 +2,7 @@ package com.sysbarbearia.api.controllers;
 
 import org.springframework.http.ResponseEntity;
 //import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,11 @@ public class RecomendacaoController {
 	@Autowired
 	private RecomendacaoService s;
 	@RequestMapping( method = RequestMethod.GET)
-	    public ResponseEntity<Void> get() {
-	    	System.out.println("\n\n\nChamando recomendacao\n\n\n");
-			s.recomedacao();
-	        return ResponseEntity.noContent().build();
-	    }
+	public ResponseEntity<Void> get() {
+		System.out.println("\n\n\nChamando recomendacao\n\n\n");
+		s = new RecomendacaoService();
+		s.recomendacaoCollaborativa(1,1);
+		return ResponseEntity.noContent().build();
+	}
 	       
 }

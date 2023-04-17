393727165d214d78a9e68c2f29c41211839c855d
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-04-17 16:28:52 -0300

    tentando fazer funcionar

--- api/src/main/java/com/sysbarbearia/api/services/RecomendacaoService.java
+++ api/src/main/java/com/sysbarbearia/api/services/RecomendacaoService.java
@@ -17,19 +17,21 @@ import com.sysbarbearia.api.algorithms.utils.MatrizBasica;
 @Service
 public class RecomendacaoService {
 	@Autowired
-	private ClienteService uservice;
+	private ClienteService uservice = new ClienteService();
 
 	@Autowired
 	private ServicoOferecidoService sservice;
-	private List<Cliente> allCliente = new ArrayList<Cliente>(uservice.findAll());
+	private List<Cliente> allCliente;
 
 
 	public void recomendacaoCollaborativa(int idEmpresa, int idCliente) {
-		List<ServicoOferecido> allServicos = new ArrayList<ServicoOferecido>(sservice.findByIdBarbearia(idEmpresa));
+		sservice = new ServicoOferecidoService();
+		List<ServicoOferecido> allServicos = new ArrayList<ServicoOferecido>(sservice.findByIdBarbearia(new Integer(1)));
+		allCliente = new ArrayList<Cliente>(uservice.findAll());
 		MatrizBasica mb = new MatrizBasica(allServicos, allCliente);
-		if(encontrarCliente(idCliente, allCliente) == null){
-			System.out.println( "O cliente informado não pode ser encontrado para ser avaliado");
-		}
+		//if(clienteEncontrado(idCliente) == null){
+			//System.out.println( "O cliente informado não pode ser encontrado para ser avaliado");
+		//}
 		MatrizSimilaridade ms = new MatrizSimilaridade(mb, encontrarCliente(idCliente, allCliente));
 		printContentMatrix(ms.getContent(), mb.getRowSize(),  mb.getColSize());
 
@@ -43,12 +45,12 @@ public class RecomendacaoService {
 		}
 		return null;
 	}
- public  Boolean clienteEncontrado(int id){
-	 if(encontrarCliente(id, allCliente) == null){
-		 return true;
-	 }
-	 return false;
- }
+ //public  Boolean clienteEncontrado(int id){
+	// if(encontrarCliente(id, allCliente) == null){
+	//	 return true;
+	 //}
+	 //return false;
+ //}
 	private void printContentMatrix(Double[][] m, int l, int c) {
 		for (int i = 0; i < l; i++) {
 			for (int j = 0; j < c; j++) {

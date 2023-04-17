3bc010293794fe3b420cc2dfb3ff732ed59f59ad
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-04-17 15:59:09 -0300

    Corrgindo erros de escrita no sql

--- api/src/main/java/com/sysbarbearia/api/repositories/ServicoOferecidoRepository.java
+++ api/src/main/java/com/sysbarbearia/api/repositories/ServicoOferecidoRepository.java
@@ -10,10 +10,10 @@ import com.sysbarbearia.api.model.ServicoOferecido;
 
 public interface ServicoOferecidoRepository extends JpaRepository<ServicoOferecido, Integer> {
 	@Transactional(readOnly = true)
-	@Query(value = "SELECT COUNT(*) FROM SERVICOS_OFERECIDOS" , nativeQuery = true)
+	@Query(value = "SELECT COUNT(*) FROM SERVICO_OFERECIDO" , nativeQuery = true)
     public Collection<?> countServicosOferecidos();
 	
 	@Transactional(readOnly = true)
-	@Query(value = "SELECT * FROM SERVICOS_OFERECIDOS sofc WHERE sofc.ID_BARBEARIA = ?1" , nativeQuery = true)
+	@Query(value = "SELECT * FROM SERVICO_OFERECIDO sofc WHERE sofc.ID_BARBEARIA = ?1" , nativeQuery = true)
 	public Collection<ServicoOferecido> findByIdBarbearia(Integer id);
 }

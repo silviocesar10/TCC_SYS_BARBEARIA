7e6a4a77c081b57b0d271a8c8bbb41b198c2a466
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-05-01 17:50:07 -0300

    Adicionando classe de coordenadas

---
+++ api/src/main/java/com/sysbarbearia/api/algorithms/utils/Coordenadas.java
@@ -0,0 +1,27 @@
+package com.sysbarbearia.api.algorithms.utils;
+
+public class Coordenadas {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Coordenadas(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}

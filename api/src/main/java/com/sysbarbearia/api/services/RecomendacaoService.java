74d1d953630ac389af0653cabe4452b747e3a5f5
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-05-08 13:42:57 -0300

    Uso service devidamente instanciado e funcionando

--- api/src/main/java/com/sysbarbearia/api/services/RecomendacaoService.java
+++ api/src/main/java/com/sysbarbearia/api/services/RecomendacaoService.java
@@ -1,15 +1,22 @@
 package com.sysbarbearia.api.services;
+//import java.util.Collection;
 import java.util.List;
 import java.util.ArrayList;
 
 
 import com.sysbarbearia.api.algorithms.utils.MatrizSimilaridade;
+import com.sysbarbearia.api.model.Uso;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.dao.DataIntegrityViolationException;
 //import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import com.sysbarbearia.api.repositories.UsoRepository;
 
 import com.sysbarbearia.api.model.ServicoOferecido;
 import com.sysbarbearia.api.model.Cliente;
@@ -24,17 +31,24 @@ public class RecomendacaoService {
 	private ServicoOferecidoService sservice;
 	private List<Cliente> allCliente;
 
-
+	//@Autowired
+	//UsoRepository repo;
+	@Autowired
+	UsoService usoService;
 	public void recomendacaoCollaborativa(Integer idEmpresa, Integer idCliente) {
 		allCliente = new ArrayList<Cliente>(uservice.findAll());
 		List<ServicoOferecido> allServicos = new ArrayList<ServicoOferecido>(sservice.findByIdBarbearia(1));
-		MatrizBasica mb = new MatrizBasica(allServicos, allCliente);
+		//MatrizBasica mb = new MatrizBasica(allServicos, allCliente);
 		//if(encontrarCliente(idCliente, allCliente) == null){
-			//System.out.println( "O cliente informado não pode ser encontrado para ser avaliado");
+		//System.out.println( "O cliente informado não pode ser encontrado para ser avaliado");
 		//}
-		MatrizSimilaridade ms = new MatrizSimilaridade(mb, encontrarCliente(idCliente, allCliente));
-		printContentMatrix(ms.getContent(), mb.getRowSize(),  mb.getColSize());
+		//MatrizSimilaridade ms = new MatrizSimilaridade(mb, encontrarCliente(idCliente, allCliente));
+		//printContentMatrix(ms.getContent(), mb.getRowSize(),  mb.getColSize());
+		//UsoService avservice = new UsoService();
+		//List<Uso> allUso = new ArrayList<Uso>(avservice.findAll());
 
+		List<Uso> allUso = new ArrayList<Uso>(usoService.findAll());
+		allUso.forEach(System.out::println);
 	}
 
 	public Cliente encontrarCliente(int id, List<Cliente> l){
@@ -51,6 +65,7 @@ public class RecomendacaoService {
 	 }
 	 return false;
  }
+
 	private void printContentMatrix(Double[][] m, int l, int c) {
 		for (int i = 0; i < l; i++) {
 			for (int j = 0; j < c; j++) {

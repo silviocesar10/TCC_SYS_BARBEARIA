616349fbc570f75460c2688fc71bb6177a08d1ac
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-05-14 11:58:28 -0300

    repository de recomendacao

---
+++ api/src/main/java/com/sysbarbearia/api/repositories/RecomendacaoRepository.java
@@ -0,0 +1,9 @@
+package com.sysbarbearia.api.repositories;
+
+
+import com.sysbarbearia.api.model.Recomendacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecomendacaoRepository  extends JpaRepository<Recomendacao, Integer> {
+
+}

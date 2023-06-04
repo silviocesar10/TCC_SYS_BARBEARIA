2a00538b20b4a0def2dcf5cab36ecc380d9cf27f
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2023-06-04 19:43:36 -0300

    Calculo de recomencao em memoria finalizado

--- api/src/main/java/com/sysbarbearia/api/services/RecomendacaoService.java
+++ api/src/main/java/com/sysbarbearia/api/services/RecomendacaoService.java
@@ -2,16 +2,16 @@ package com.sysbarbearia.api.services;
 //import java.util.Collection;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 import com.sysbarbearia.api.algorithms.utils.MatrizSimilaridade;
 import com.sysbarbearia.api.algorithms.utils.PosicaoCelula;
-import com.sysbarbearia.api.model.Recomendacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import com.sysbarbearia.api.services.exceptions.BusinessRuleException;
 import com.sysbarbearia.api.model.ServicoOferecido;
 import com.sysbarbearia.api.model.Cliente;
 import com.sysbarbearia.api.model.Uso;
@@ -36,20 +36,21 @@ public class RecomendacaoService {
 	@Autowired
 	UsoService usoService;
 
-	public void recomendacaoCollaborativa(Integer idEmpresa, Integer idCliente) {
+	public List<ServicoOferecido> recomendacaoCollaborativa(Integer idEmpresa, Integer idCliente) {
 		List<Uso> allUso = new ArrayList<Uso>(usoService.findAll());
 		allUso.forEach(System.out::println);
 		allCliente = new ArrayList<Cliente>(clienteService.findAll());
-		List<ServicoOferecido> allServicos = new ArrayList<ServicoOferecido>(servicoOferecidoService.findByIdBarbearia(1));
+		List<ServicoOferecido> allServicos = new ArrayList<ServicoOferecido>(servicoOferecidoService.findByIdBarbearia(idEmpresa));
 		MatrizBasica mb = new MatrizBasica(allServicos, allCliente, allUso);
 		printContentMatrix(mb.getContent(), mb.getRowSize(),  mb.getColSize());
-		//if(encontrarCliente(idCliente, allCliente) == null){
-		//System.out.println( "O cliente informado não pode ser encontrado para ser avaliado");
-		//}
-		MatrizSimilaridade ms = new MatrizSimilaridade(mb, encontrarCliente(1, allCliente));
+		MatrizSimilaridade ms = new MatrizSimilaridade(mb, encontrarCliente(idCliente, allCliente));
 		printContentMatrix(ms.getContent(), ms.getRowSize(),  ms.getColSize());
-
-
+		ArrayList<PosicaoCelula> pc = new ArrayList<PosicaoCelula>();
+		List<ServicoOferecido> lista = calcular(ms.getColSize(), ms.getRowSize(),ms.getContent(),pc, ms.getLinhaUsuario());
+		if(lista.isEmpty()){
+			throw new BusinessRuleException("Não ha recomendações disponiveis para o usuario alvo " + encontrarCliente(idCliente, allCliente).getNome());
+		}
+		return lista;
 	}
 
 	public List<ServicoOferecido> calcular(int col, int rol, Double [][] m, List<PosicaoCelula> pc, int linhaUsuario){
@@ -63,23 +64,36 @@ public class RecomendacaoService {
 		}
 		List<ServicoOferecido> lista = new ArrayList<ServicoOferecido>();
 		Integer tmp;
-		PosicaoCelula este = pc.stream().findFirst().get();
-		tmp = pc.stream().findFirst().get().getColuna();
-		lista.remove(este);
-		while(este != null){
-			ServicoOferecido s = multiplicar(m, linhaUsuario, tmp, colDistancia + 1);
-			if(s != null){
-				lista.add(s);
+		do{
+			for (PosicaoCelula n : pc) {
+				tmp = n.getColuna();
+				ServicoOferecido s = multiplicar(m, linhaUsuario, tmp, col, rol);
+				if (s != null) {
+					lista.add(s);
+				}
+
 			}
-		}
+			pc.remove(0);
+		}while (pc.size() > 0);
 		return lista;
 
 	}
-	private ServicoOferecido multiplicar(Double m[][], int linhaUsuario, int col, int colDistancia){
+	private ServicoOferecido multiplicar(Double m[][], int linhaUsuario, int col, int colDistancia, int rol){
 		Double soma = 0.0d;
-		for (int h = col ; h < col; h++){
-			soma += m[h][col] * m[h][colDistancia];
+		Double simi = 0.0d;
+		for (int i = 0 ; i < rol ; i++){
+				if(m[i][col] == null){
+					soma = soma + 0.0d;
+				}else{
+					soma += m[i][col] * m[i][colDistancia - 1];
+					simi = simi + m[i][colDistancia - 1];
+					System.out.println("soma aqui" + soma);
+				}
+
+
 		}
+		soma = soma / simi;
+		System.out.println("soma aqui" + soma);
 		if(soma >= NOTADECORTE){
 			System.out.println("vai ser recomendado");
 			return servicoOferecidoService.findById(col + 1);
